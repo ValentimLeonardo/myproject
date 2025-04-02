21f4206bf413946f2a1fc0e103ff2383bd0b24e9
Leonardo <leonardo.valentim.dos.santos.2@gmail.com>
2025-04-02 19:41:18 -0300

    finalização do projeto

--- App.js
+++ App.js
@@ -55,10 +55,15 @@ const Cadastro= () => {
       flexGrow: 1,
     }}
 >
-      <Avatar
-        size="large"
-        rounded
-        source={{ uri: 'C:\Users\Aluno\Desktop\Nova pasta\myproject\imagem\download.jfif' }}
+      <Text containerStyle={{
+          fontSize: 32, 
+      }}>CADASTRO</Text>
+      <Text>Nome</Text>
+      <Input containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,}}
+        placeholder='BASIC INPUT'
       />
       <Text>Email</Text>
       <Input containerStyle={{
@@ -74,17 +79,41 @@ const Cadastro= () => {
                 marginVertical: 10,}}
         placeholder='BASIC INPUT'
       />
-    
      <Button containerStyle={{
                 width: 200,
                 marginHorizontal: 50,
-                marginVertical: 10,}} >Logar</Button>
+                marginVertical: 10,}}>Cadastro</Button>
+    </View>
+  );
+};
+const Recuperar= () => {
+
+  return (
+    <View style={{
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 1,
+      flexGrow: 1,
+    }}
+>
+      <Text containerStyle={{
+          fontSize: 32, 
+      }}>ESQUECI A SENHA</Text>
+      <Text>Email</Text>
+      <Input containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,}}
+        placeholder='BASIC INPUT'
+      />
      <Button containerStyle={{
                 width: 200,
                 marginHorizontal: 50,
-                marginVertical: 10,}}>Esqueci cadastro</Button>
+                marginVertical: 10,}}>Enviar</Button>
     </View>
   );
 };
 
-export default Tela;
+export default Recuperar;
+//export default login;
+//export default Cadastro;
