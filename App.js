e83cd08588342585bad1f27c396888ecce3ad2c8
DESKTOP-7M01SED\Aluno <aluno@jaboatao.ifpe.edu.br>
2025-04-01 21:05:11 -0300

    atividade2

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Avatar } from 'react-native-elements';
 import { View, Text } from 'react-native';
 import { Input} from '@rneui/themed';
 import { Button } from '@rneui/themed';
-export default () => {
+
+const Login = () => {
 
   return (
     <View style={{
@@ -12,7 +14,11 @@ export default () => {
       flexGrow: 1,
     }}
 >
-
+      <Avatar
+        size="large"
+        rounded
+        source={{ uri: 'C:\Users\Aluno\Desktop\Nova pasta\myproject\imagem\download.jfif' }}
+      />
       <Text>Email</Text>
       <Input containerStyle={{
                 width: 300,
@@ -39,3 +45,46 @@ export default () => {
     </View>
   );
 };
+const Cadastro= () => {
+
+  return (
+    <View style={{
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 1,
+      flexGrow: 1,
+    }}
+>
+      <Avatar
+        size="large"
+        rounded
+        source={{ uri: 'C:\Users\Aluno\Desktop\Nova pasta\myproject\imagem\download.jfif' }}
+      />
+      <Text>Email</Text>
+      <Input containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,}}
+        placeholder='BASIC INPUT'
+      />
+      <Text>Senha</Text>
+      <Input containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,}}
+        placeholder='BASIC INPUT'
+      />
+    
+     <Button containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,}} >Logar</Button>
+     <Button containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,}}>Esqueci cadastro</Button>
+    </View>
+  );
+};
+
+export default Tela;
